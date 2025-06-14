c57840544cb050ed38b6a517f98bd355d8a4ce38
Aiyern30 <ianbian2@gmail.com>
2025-06-15 01:33:28 +0800

    created types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,26 @@
+interface Window {
+  Spotify?: {
+    Player: new (options: {
+      name: string;
+      getOAuthToken: (cb: (token: string) => void) => void;
+      volume?: number;
+    }) => {
+      connect: () => Promise<boolean>;
+      disconnect: () => void;
+      addListener: (event: string, callback: (data: any) => void) => void;
+      removeListener: (event: string, callback?: (data: any) => void) => void;
+      getCurrentState: () => Promise<any>;
+      setName: (name: string) => Promise<void>;
+      getVolume: () => Promise<number>;
+      setVolume: (volume: number) => Promise<void>;
+      pause: () => Promise<void>;
+      resume: () => Promise<void>;
+      togglePlay: () => Promise<void>;
+      seek: (position_ms: number) => Promise<void>;
+      previousTrack: () => Promise<void>;
+      nextTrack: () => Promise<void>;
+      activateElement: () => Promise<void>;
+    };
+  };
+  onSpotifyWebPlaybackSDKReady?: () => void;
+}
