430a4134c054a16d5aff7b412ca99ce00e039cfd
Aiyern30 <ianbian2@gmail.com>
2025-06-14 20:55:05 +0800

    Added handle widescreen design

---
+++ src/hooks/use-widescreen.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const BREAKPOINT = 1800;
+
+export function useWideScreen() {
+  const [isWideScreen, setIsWideScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > BREAKPOINT);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+}
